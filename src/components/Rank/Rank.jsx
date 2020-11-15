00ada5cdac28ada5c8be3eb6df8dbd17312b35ef
Nameer-kp <kpnameer@yahoo.com>
2020-11-16 01:20:10 +0530

    added particles js

---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Rank =()=>{
+    return(
+
+        <div>
+
+            <div className='white f3'>
+                {'Hey User your current Rank is..'}
+            </div>
+            <div className='white f1'>
+                {'No .6'}
+            </div>
+
+
+        </div>
+
+    );
+};
+
+export default Rank;
