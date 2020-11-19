75af649501f8579bf9989d3646bc7fd50ec23589
Nameer-kp <kpnameer@yahoo.com>
2020-11-20 02:00:02 +0530

    added facebox

--- src/App.js
+++ src/App.js
@@ -40,8 +40,34 @@ class App extends Component {
     this.state={
       input:'',
       imageUrl:'',
+      box:{}
     }
   }
+
+  calculateFaceLocation = (data) => {
+
+    const clarifaiFace= data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image= document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height =Number(image.height);
+    console.log("height",height);
+
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width -(clarifaiFace.right_col*width),
+      bottomRow: height-(clarifaiFace.bottom_row*height)
+
+
+    }
+
+  }
+
+  displayFaceBox =(box)=>{
+    console.log(box);
+    this.setState({box:box});
+  }
+
   onInputChange=(event)=>{
     
     this.setState({input:event.target.value});
@@ -66,7 +92,8 @@ class App extends Component {
   callToApi=()=>{
     app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.imageUrl)
   .then(response => {
-    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    //here the processed response is passed to displayFaceBox
+    this.displayFaceBox(this.calculateFaceLocation(response));
     // There was a successful response
   })
   .catch(error => {
@@ -84,7 +111,7 @@ class App extends Component {
       <Logo/>
       <Rank/>
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/> 
-      <FaceRecognition imageUrl={this.state.imageUrl}/>
+      <FaceRecognition box ={this.state.box} imageUrl={this.state.imageUrl}/>
 
       
     </div>
--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import  './FaceRecognition.css'
 
-const FaceRecognition =({imageUrl}) =>{
+const FaceRecognition =({imageUrl,box}) =>{
 
     return(
         <div className='center ma'>
 
-            <div absolute mt2>
+            <div absolute mt2 style={{marginTop:'50px',width:'500px',height:'auto'}}>
                 {console.log("onFaceRecognition")}
 
-                   <img src={imageUrl} alt="" width='500px' height='auto' style={{marginTop:'50px'}}/>
+                   <img id='inputimage' src={imageUrl} alt=""/>
+                    <div className='bounding-box' style={{top:box.topRow,right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}>
 
+                    </div>
             </div>
+            
             
 
         </div>
